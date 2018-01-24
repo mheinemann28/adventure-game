ad6e7cd64c69e8dabbe1c45e674215c970437e5f
Heidi Uphoff <hauphoff@gmail.com>
2018-01-24 10:14:50 -0700

    added header conditional

--- GameEngine_helpers.h
+++ GameEngine_helpers.h
@@ -8,3 +8,15 @@
  ** Output: none
  *********************************************************************/
  
+/* To prevent compiler from processing contents twice, wrap in a conditional */
+#ifndef GAMEENGINEHELPERS_H
+#define GAMEENGINEHELPERS_H
+
+
+
+
+
+
+
+/* Place at end of file - End of header conditional */
+#endif
