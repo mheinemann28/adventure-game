bd8681996c2e96b2f6438c2b673f078b0ae66a2d
Heidi Uphoff <hauphoff@gmail.com>
2018-01-24 09:54:45 -0700

    Added stub code for CommandParser testing program

---
+++ CommandParser-tester.c
@@ -0,0 +1,23 @@
+/*********************************************************************
+ ** Program Filename: CommandParser-tester.c 
+ ** Author: Adventure Team (Michael Heinemann, Stephen Liu, Heidi Uphoff)
+ ** Date: Jan. 24, 2018
+ ** Description: tests CommandParser.h without running GameEngine.c
+ ** Input: command line input
+ ** Output: parsed response
+ *********************************************************************/
+
+#include "CommandParser-tester.h"
+#include "CommandParser.h"
+ 
+/*********************************************************************
+ ** Function: main();
+ ** Description: starts command parser testing program
+ ** Parameters: none
+ ** Pre-Conditions: none
+ ** Post-Conditions: program exits
+ *********************************************************************/
+ 
+ int main() {
+    return 0;
+}
---
+++ CommandParser-tester.h
@@ -0,0 +1,10 @@
+/*********************************************************************
+ ** Program Filename: CommandParser-tester.h 
+ ** Author: Adventure Team (Michael Heinemann, Stephen Liu, Heidi Uphoff)
+ ** Date: Jan. 24, 2018
+ ** Description: tests CommandParser.h without running GameEngine.c
+ ** Input: command line input
+ ** Output: parsed response
+ *********************************************************************/
+ 
+ 
